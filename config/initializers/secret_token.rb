799a800498eace9e629018dfd5f6da1b4eba96d2
Tommaso Visconti <tommaso.visconti@gmail.com>
2012-01-23 19:51:20 +0100

    Rails 3.2.0
    Signed-off-by: Tommaso Visconti <tommaso.visconti@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Easyfatt::Application.config.secret_token = '5593689558b24691797ee62bb67081b8329edbc70f3ed5b0bf5fdde72b85b28d6ecc49b13a0d30c2ead44f4539f3c0f8a488d34aff5e684d77be867c1535a36d'
