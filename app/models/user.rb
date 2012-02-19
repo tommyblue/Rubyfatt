e02d7a5e1d5a4b432f5ac9d3550dec9b1d482d1f
Tommaso Visconti <tommaso.visconti@gmail.com>
2012-02-19 17:00:01 +0100

    Clear useless modules of Devise
    Signed-off-by: Tommaso Visconti <tommaso.visconti@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :registerable
+  devise :database_authenticatable, :recoverable, :rememberable#, :trackable, :validatable, :registerable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :surname, :address, :zip_code, :town, :province, :country, :tax_code, :vat, :phone
