56c7c04b2e5604013affc596ac736a8790ab2ca8
Tommaso Visconti <tommaso.visconti@gmail.com>
2012-03-07 22:56:52 +0100

    Added tinycon to show the expired recurring slips in the favicon
    Signed-off-by: Tommaso Visconti <tommaso.visconti@gmail.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
-  def expired_recurring_slips
+  def expired_recurring_slips(only_num = false)
     expired = RecurringSlip.expired
-    expired > 0 ? '<span class="circle">' + expired.to_s + '</span>' : ''
+    if only_num
+      expired
+    else
+      expired > 0 ? '<span class="circle">' + expired.to_s + '</span>' : ''
+    end
   end
 end
