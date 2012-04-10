c6afe76e7b7b9fcd659f641686b646b74fc507f3
Tommaso Visconti <tommaso.visconti@gmail.com>
2012-04-10 11:21:30 +0200

    Fixed Issue #2

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,7 +1,9 @@
 module InvoicesHelper
   def get_selectable_invoices_years
     years = []
-    current_user.invoices.select('YEAR(date) AS year').group('YEAR(date)').each { |group| years << group.year }
+    # Had to disable this method because Sqlite doesn't like it... :(
+    #current_user.invoices.select('YEAR(date) AS year').group('YEAR(date)').each { |group| years << group.year }
+    current_user.invoices.each { |invoice| (years << invoice.date.year) unless years.include?(invoice.date.year) }
     years
   end
 end
