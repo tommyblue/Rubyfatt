e44762d2946218ac20cd940d9005b4e759f89aaf
Tommaso Visconti <tommaso.visconti@gmail.com>
2012-04-23 12:24:21 +0200

    Propagata la correzione di Invoice anche a Estimate

--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -2,26 +2,26 @@ class Estimate < ActiveRecord::Base
   belongs_to :consolidated_tax
   belongs_to :customer
   has_many :slips
-  
+
   validates_presence_of :date
-  
-  before_save do
+
+  before_create do
     self.number = self.customer.user.options.where(:name => 'NEXT_ESTIMATE_NUMBER').first.value.to_i
   end
-    
-  after_save do
+
+  after_create do
     opt = self.customer.user.options.where(:name => 'NEXT_ESTIMATE_NUMBER').first
     opt.value = opt.value.to_i + 1
     opt.save!
   end
-  
+
   # Get the sum of the slips' rates
   def rate
     sum = 0
     self.slips.each { |slip| sum += slip.rate }
     sum
   end
-  
+
   # Applies consolidated taxes to the slip rate
   def total
     sum = self.rate
@@ -35,7 +35,7 @@ class Estimate < ActiveRecord::Base
         sum += partial
       end
     end
-    
+
      compounds.each { |compound| sum += compound }
     sum
   end
