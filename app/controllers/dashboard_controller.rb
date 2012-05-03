6e61311b4b77bc68c41fcea48ba84f4002f7c735
Tommaso Visconti <tommaso.visconti@gmail.com>
2012-05-03 11:31:06 +0200

    0.10
    Signed-off-by: Tommaso Visconti <tommaso.visconti@gmail.com>

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,5 +5,8 @@ class DashboardController < ApplicationController
   def index
     @customers = current_user.customers
     @year_invoices = current_user.invoices.by_year(Time.now.year)
+    @this_year = current_user.invoices.sum("slips.rate").joins(:slips).by_year(Time.now.year).group("MONTH(invoices.date)")
+    # select MONTH(i.date),sum(s.rate) from slips as s join invoices as i on s.invoice_id=i.id where YEAR(i.date)=2012 group by MONTH(i.date)
+    # Invoice.joins(:slips).where("YEAR(invoices.date) = ?", Time.now.year).group("MONTH(invoices.date)")
   end
 end
