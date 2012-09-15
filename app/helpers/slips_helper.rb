140eab35ff0d80a9b2aaf975900c77a01b19edda
Tommaso Visconti <tommaso.visconti@gmail.com>
2012-09-16 00:58:17 +0200

    Fixes #44

--- app/helpers/slips_helper.rb
+++ app/helpers/slips_helper.rb
@@ -1,2 +1,9 @@
 module SlipsHelper
+  def check_slip_duration(slip)
+    if slip.timed
+      slip.total_hours > slip.duration ? "<span class=\"label label-important\">#{slip.duration}</span>" : "#{slip.duration}"
+    else
+      '-'
+    end
+  end
 end
