f1b1fb624e9abb8bf9c85aa0cdba3729571432ed
Tommaso Visconti <tommaso.visconti@gmail.com>
2014-02-24 22:33:07 +0100

    Show new year warning only to logged in users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ class ApplicationController < ActionController::Base
 
   # If the invoice number is > 1 but this year there are no invoices, probably this is a new year
   def check_new_year
-    if Option.get_option_value(User.first, 'NEXT_INVOICE_NUMBER') > 1 && Invoice.where("#{DbAdapter.get_year "invoices.date"} = ?",Time.now.year).count == 0
+    if current_user && Option.get_option_value(current_user, 'NEXT_INVOICE_NUMBER') > 1 && current_user.invoices.where("#{DbAdapter.get_year "invoices.date"} = ?",Time.now.year).count == 0
       flash.now[:info] = '<i class="icon-info-sign"></i> ' + I18n.t(:new_year_message, default: "No invoices present for the current year, but the next invoice number isn't 1, maybe you need to reset the counters from the option's page")
     end
   end
