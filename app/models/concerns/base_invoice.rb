39b4f88f4ebe0fece937a8e5731f2cbd52054653
Tommaso Visconti <tommaso.visconti@gmail.com>
2014-03-25 22:58:49 +0100

    moved BaseInvoice module into models/concerns

--- app/models/concerns/base_invoice.rb
+++ app/models/concerns/base_invoice.rb
@@ -12,6 +12,7 @@ module BaseInvoice
 
     base.send :default_scope, Proc.new { base.order DbAdapter.get_year("#{base.table_name}.date"), "#{base.table_name}.number", "#{base.table_name}.id" }
     base.send :scope, :sorted, -> { base.order('date DESC') }
+    base.send :scope, :ordered, -> { base.order('date DESC') }
     base.send :scope, :by_year, lambda { |year| base.where("date >= ? and date <= ?", "#{year}-01-01", "#{year}-12-31") }
 
     base.before_create do |base_invoice|
