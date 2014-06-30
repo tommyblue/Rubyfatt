30a597dea87a019608f254f7f20be98cde12e9b1
Tommaso Visconti <tommaso.visconti@gmail.com>
2014-06-30 16:11:40 +0200

    Coveralls code for Travis CI
    Signed-off-by: Tommaso Visconti <tommaso.visconti@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
+
+require 'coveralls'
+Coveralls.wear!
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
